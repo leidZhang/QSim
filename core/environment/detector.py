679d71e2c4b33b189c52ac4950a595f147ef4eaf
leidZhang <haoz28889@gmail.com>
2024-05-16 22:11:23 -0400

    unit test

--- core/environment/detector.py
+++ core/environment/detector.py
@@ -4,17 +4,19 @@ import numpy as np
 
 from .exception import AnomalousEpisodeException
 
+
 class EpisodeMonitor:
     def __init__(self, start_orig: np.ndarray) -> None:
         self.queue: Queue = Queue(10)
         self.start_orig: np.ndarray = start_orig
         self.accumulator: int = 0
-    
-    def __call__(self, action: np.ndarray, orig: np.ndarray) -> None: 
+        print("aaa")
+
+    def __call__(self, action: np.ndarray, orig: np.ndarray) -> None:
         result: int = 1
         if action[0] >= 0.045 and np.array_equal(orig, self.start_orig):
             result: int = 0
-        
+
         if self.queue.full():
             self.accumulator -= self.queue.get()
         self.queue.put(result)
