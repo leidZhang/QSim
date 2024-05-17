b53ee7b4cd92564f691b224355c74d1fb8a5c975
leidZhang <haoz28889@gmail.com>
2024-05-17 13:29:13 -0400

    update car

--- core/qcar/vehicle.py
+++ core/qcar/vehicle.py
@@ -1,3 +1,4 @@
+from abc import abstractmethod
 from typing import Union, Tuple
 
 import numpy as np
@@ -9,19 +10,23 @@ from core.base_policy import PolicyAdapter, BasePolicy
 from .monitor import Monitor
 
 
-class BaseCar: 
+class BaseCar:
+    def __init__(self, throttle_coeff: float, steering_coeff: float) -> None:
+        self.throttle_coeff: float = throttle_coeff
+        self.steering_coeff: float = steering_coeff
+
+    @abstractmethod
+    def execute(self, *args) -> Tuple:
+        ...
+
+
+class VirtualCar(BaseCar): 
     def __init__(self, actor_id, dt, throttle_coeff: float = 0.3, steering_coeff: float = 0.5) -> None:
         # basic attributes
+        super().__init__(throttle_coeff, steering_coeff)
         self.actor_id: int = actor_id
         self.running_gear: QCar = QCar(id=actor_id)
         self.monitor: Monitor = Monitor(160, actor_id, dt=dt)
-        self.throttle_coeff: float = throttle_coeff
-        self.steering_coeff: float = steering_coeff
-        # custom attributes
-        self.policy = None
-
-    def set_policy(self, policy: Union[PolicyAdapter, BasePolicy]) -> None: 
-        self.policy = policy
 
     def get_ego_state(self) -> np.ndarray:
         return self.monitor.get_state()
@@ -35,9 +40,22 @@ class BaseCar:
         ])
         return orig, yaw, rot
 
-    def execute(self, observation: dict) -> None: 
-        action, metrics = self.policy.execute(observation)
+    def execute(self, action: np.ndarray) -> Tuple[float, float]: 
         throttle: float = self.throttle_coeff * action[0]
         steering: float = self.steering_coeff * action[1]
         self.running_gear.read_write_std(throttle, steering)
-        return action, metrics
+        return throttle, steering
+    
+
+class VirtualAgentCar(VirtualCar): 
+    def __init__(self, actor_id, dt, throttle_coeff: float = 0.3, steering_coeff: float = 0.5) -> None:
+        super().__init__(actor_id, dt, throttle_coeff, steering_coeff)
+        self.policy: Union[BasePolicy, PolicyAdapter] = None
+        
+    def set_policy(self, policy: Union[BasePolicy, PolicyAdapter]) -> None:
+        self.policy = policy
+
+    def execute(self, observation: dict) -> Tuple[float, float]: 
+        action, _ = self.policy.execute(observation)
+        return super().execute(action)
+    
