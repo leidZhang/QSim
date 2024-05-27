06583caf893aa79c510811e73ab0147da7b6e899
leidZhang <haoz28889@gmail.com>
2024-05-27 16:00:18 -0400

    update

--- run_demo.py
+++ run_demo.py
@@ -1,9 +1,6 @@
 import cv2
 
 from demos.simulator_demo import simulator_demo
-from demos.pid_demo.throttle_pid_demo import throttle_pid_demo
-from demos.pid_demo.steering_pid_demo import steering_pid_demo
-from demos.pid_demo.close_loop_demo import vision_pid_demo
 
 if __name__ == "__main__":
-    vision_pid_demo()
+    simulator_demo()
