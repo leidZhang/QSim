30486b8d15fec63a16081565c9766300b230eab9
leidZhang <haoz28889@gmail.com>
2024-06-15 11:48:48 -0400

    Signed-off-by: leidZhang <haoz28889@gmail.com>

--- tests/unit_test/test_filters.py
+++ tests/unit_test/test_filters.py
@@ -26,7 +26,7 @@ def test_variable_threshold_filter_1() -> None:
         reduce_factor=0.4
     )
     filtered_signals: List[float] = []
-    input_signals: List[float] = [0, 0.3, 0.4, 0, 0.5, 0.55, 0.03, 0.59, 0.05, 0.63, 0.14, 0.62, 0]
+    input_signals: List[float] = [0, 0.3, 0.4, 0, 0.5, 0.55, 0.03, 0.59, 0.05, 0.63, 0.14, 0, 0.62, 0]
     for signal in input_signals:
         filtered_signals.append(filter(signal))
 
