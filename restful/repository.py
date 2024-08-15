b7756b85af4f1173d7e632e5a7dcc01ee465b6e2
creak77 <mayiqun.edwin@gmail.com>
2024-08-15 14:51:49 -0400

    test for npz generation

--- restful/repository.py
+++ restful/repository.py
@@ -44,17 +44,19 @@ class DataRepository(IDataRepository): # DataModel
         logging.info(f"Added step data: {step_data}")
 
     def handle_episode_complete(self) -> None:
+        print("Starting to pack the episode data...")
         episode_uid: str = str(uuid.uuid1(int(time.time() * 1000)))
-        filename: str = os.path.join(NPZ_DIR, f"{episode_uid}.npz")
+        filename: str = os.path.join(NPZ_DIR, f"{episode_uid}")
         self.episode_data["reward"] = deepcopy(self.rewards)
         self.episode_data["action"] = self.actions
         interventions: np.ndarray = self.episode_data["intervention"]
         for key, val in self.episode_data.items():
             if type(val) is not list:
-                return 
+                continue
+
             if len(val) > len(self.rewards):
                 self.episode_data[key] = val[:len(self.rewards)+1]
-        # calculate the agent reward
+        # calculate the agent reward    
         for i in range(len(self.episode_data["reward"])):
             self.episode_data["reward"][i] = self.episode_data["reward"][i] - HITL_REWARD * interventions[i]
                 
@@ -62,7 +64,7 @@ class DataRepository(IDataRepository): # DataModel
         print("Last step reward: ", self.episode_data["reward"][-1])                
         print(f"Episode reward: {sum(self.episode_data['reward'])}")
         self.episode_data["sentinel"] = True  # for data integrity checking
-        with open(f"agent_{filename}", 'wb') as f:
+        with open(f"{filename}_agent.npz", 'wb') as f:
             np.savez(f, **self.episode_data)
         logging.info(f"{len(self.episode_data['image'])} steps written to agent_{filename}")
 
@@ -70,7 +72,7 @@ class DataRepository(IDataRepository): # DataModel
             for i, intervention in enumerate(interventions):
                 self.episode_data["action"][i][0] = self.episode_data["action"][i][0] * (1 - intervention)
                 self.episode_data["reward"][i] = self.rewards[i] + HITL_REWARD * interventions[i]
-            with open(f"human_{filename}", 'wb') as f:
+            with open(f"{filename}_human.npz", 'wb') as f:
                 np.savez(f, **self.episode_data)
             logging.info(f"{len(self.episode_data['image'])} steps written to human_{filename}")
         
@@ -78,5 +80,5 @@ class DataRepository(IDataRepository): # DataModel
         self.rewards = []
         self.actions = []
 
-        return f"agent_{filename}"
+        return f"{filename}"
     
