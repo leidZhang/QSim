c212482c596554cd0422f6ec654d449921e9e29d
creak77 <mayiqun.edwin@gmail.com>
2025-07-17 01:01:26 -0400

    refactor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 # from tests.unit_test.test_dispatcher import test_dispatcher_1
-from Reinformer.test import test_reinformer_car
-from Reinformer.utils import test_reinformer_util
 # from tests.unit_test.test_roadmap import test_generate_path_1
 from demos.pure_pursuit.test import test_purepursuite_car
 from demos.pure_pursuit.test import test_mock_optitrack_client
